e5695533e0461f9082d85e42f299be00be8205d5
Nikita! <141866188+Recanos@users.noreply.github.com>
2024-04-28 20:15:40 +0300

    Add files via upload

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Articles
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ArticlesAPITestCase(APITestCase):
+
+    def setUp(self):
+        # Создаем тестового пользователя и сохраняем его в базе данных
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client = APIClient()
+
+    def test_create_article(self):
+        # Тест на создание новой статьи
+        self.client.force_authenticate(self.user)  # Проходим аутентификацию
+        data = {'header': 'New Article', 'body': 'A great story!'}
+        response = self.client.post(reverse('articles-list'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_article_list(self):
+        # Тест на получение списка статей без аутентификации
+        response = self.client.get(reverse('articles-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_registration(self):
+        # Тест на регистрацию пользователя
+        data = {'username': 'newuser', 'password': 'newpassword123'}
+        response = self.client.post(reverse('user_register'), data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('access' in response.data)
+        self.assertTrue(User.objects.filter(username='newuser').exists())
+
+    def test_jwt_obtain_pair(self):
+        # Тест на получение JWT токенов для аутентифицированного пользователя
+        response = self.client.post(reverse('token_obtain_pair'), 
+                                    {'username': 'testuser', 'password': 'testpassword'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('access' in response.data)
+        self.assertTrue('refresh' in response.data)
+
